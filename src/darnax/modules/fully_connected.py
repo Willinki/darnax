fix: reset lr scaling for sparse modules

src/darnax/modules/fully_connected.py
@@ -280,7 +280,7 @@ class SparseFullyConnected(FullyConnected):
         key: Array,
         dtype: DTypeLike = jnp.float32,
         *,
-        lr: float = 1.0,
+        lr: float | None = None,
         weight_decay: float = 0.0,
     ):
         """Initialize sparse mask and masked weight matrix.
@@ -320,7 +320,9 @@ class SparseFullyConnected(FullyConnected):
         """
         self.strength = self._set_shape(strength, out_features, dtype)
         self.threshold = self._set_shape(threshold, out_features, dtype)
-        self.lr = jnp.asarray(lr, dtype=dtype)
+        self.lr = jnp.asarray(
+            1.0 if lr is None else lr * (0.1**0.5) / (1 - sparsity) ** 0.5, dtype=dtype
+        )
 
         # reproducibility introduces a rescaling
         wd_rescaling = (0.1**0.5) / (((1 - sparsity) * in_features) ** 0.5)
@@ -372,8 +374,8 @@ class SparseFullyConnected(FullyConnected):
 
         """
         grad = perceptron_rule_backward(x, y, y_hat, self.threshold, gate)
+        grad = grad * self._mask
         dW = self.lr * grad + self.weight_decay * self.lr * self.W
-        dW = dW * self._mask
         zero_update = jax.tree.map(jnp.zeros_like, self)
         new_self: Self = eqx.tree_at(lambda m: m.W, zero_update, dW)
         return new_self

src/darnax/modules/recurrent.py
@@ -94,7 +94,7 @@ class RecurrentDiscrete(Layer):
         strength: float = 1.0,
         dtype: DTypeLike = jnp.float32,
         *,
-        lr: float = 1.0,
+        lr: float | None = None,
         weight_decay: float = 0.0,
     ):
         """Construct the layer parameters.
@@ -137,7 +137,7 @@ class RecurrentDiscrete(Layer):
         j_d_vec = self._set_shape(j_d, features, dtype)
         thresh_vec = self._set_shape(threshold, features, dtype)
         strength_vec = jnp.asarray(strength, dtype=dtype)
-        self.lr = jnp.asarray(lr, dtype=dtype)
+        self.lr = jnp.asarray(1.0 if lr is None else lr, dtype=dtype)
         self.weight_decay = jnp.asarray(weight_decay / (features**0.5), dtype=dtype)
 
         J = (
@@ -262,8 +262,8 @@ class RecurrentDiscrete(Layer):
         if gate is None:
             gate = jnp.array(1.0)
         dJ = perceptron_rule_backward(x, y, y_hat, self.threshold, gate)
-        dJ = self.lr * dJ + self.lr * self.weight_decay * self.J
         dJ = dJ * self._mask
+        dJ = self.lr * dJ + self.lr * self.weight_decay * self.J
         zero_update = jax.tree.map(jnp.zeros_like, self)
         new_self: Self = eqx.tree_at(lambda m: m.J, zero_update, dJ)
         return new_self
@@ -349,7 +349,7 @@ class SparseRecurrentDiscrete(Layer):
         strength: float = 1.0,
         dtype: DTypeLike = jnp.float32,
         *,
-        lr: float = 1.0,
+        lr: float | None = None,
         weight_decay: float = 0.0,
     ):
         """Construct the layer parameters.
@@ -412,7 +412,9 @@ class SparseRecurrentDiscrete(Layer):
         self.J_D = j_d_vec
         self.threshold = thresh_vec
         self.strength = strength_vec
-        self.lr = jnp.asarray(lr, dtype=dtype)
+        self.lr = jnp.asarray(
+            1.0 if lr is None else lr * (0.01**0.5) / (1 - sparsity) ** 0.5, dtype=dtype
+        )
         self.weight_decay = jnp.asarray(weight_decay * wd_rescaling, dtype=dtype)
         self._mask = mask
 
@@ -521,8 +523,8 @@ class SparseRecurrentDiscrete(Layer):
 
         """
         dJ = perceptron_rule_backward(x, y, y_hat, self.threshold, gate)
-        dJ = self.lr * dJ + self.lr * self.weight_decay * self.J
         dJ = dJ * self._mask
+        dJ = self.lr * dJ + self.lr * self.weight_decay * self.J
         zero_update = jax.tree.map(jnp.zeros_like, self)
         new_self: Self = eqx.tree_at(lambda m: m.J, zero_update, dJ)
         return new_self

src/darnax/modules/recurrent_tanh.py
@@ -98,7 +98,7 @@ class RecurrentTanh(Layer):
         strength: float = 1.0,
         dtype: DTypeLike = jnp.float32,
         *,
-        lr: float = 1.0,
+        lr: float | None = None,
         weight_decay: float = 0.0,
     ):
         """Construct the layer parameters.
@@ -141,7 +141,7 @@ class RecurrentTanh(Layer):
         j_d_vec = self._set_shape(j_d, features, dtype)
         tol_vec = self._set_shape(tolerance, features, dtype)
         strength_vec = jnp.asarray(strength, dtype=dtype)
-        self.lr = jnp.asarray(lr, dtype=dtype)
+        self.lr = jnp.asarray(1.0 if lr is None else lr, dtype=dtype)
         self.weight_decay = jnp.asarray(weight_decay / (features**0.5), dtype=dtype)
 
         J = (
@@ -265,8 +265,8 @@ class RecurrentTanh(Layer):
         if gate is None:
             gate = jnp.array(1.0)
         dJ = tanh_perceptron_rule_backward(x, y, y_hat, self.tolerance)
-        dJ = self.lr * dJ + self.lr * self.weight_decay * self.J
         dJ = dJ * self._mask
+        dJ = self.lr * dJ + self.lr * self.weight_decay * self.J
         zero_update = jax.tree.map(jnp.zeros_like, self)
         new_self: Self = eqx.tree_at(lambda m: m.J, zero_update, dJ)
         return new_self
@@ -359,7 +359,7 @@ class RecurrentTanhTruncated(RecurrentTanh):
         strength: float = 1.0,
         dtype: DTypeLike = jnp.float32,
         *,
-        lr: float = 1.0,
+        lr: float | None = None,
         weight_decay: float = 0.0,
     ):
         """Construct the layer parameters.
@@ -457,8 +457,8 @@ class RecurrentTanhTruncated(RecurrentTanh):
         if gate is None:
             gate = jnp.array(1.0)
         dJ = tanh_truncated_perceptron_rule_backward(x, y, y_hat, self.threshold, self.tolerance)
-        dJ = self.lr * dJ + self.lr * self.weight_decay * self.J
         dJ = dJ * self._mask
+        dJ = self.lr * dJ + self.lr * self.weight_decay * self.J
         zero_update = jax.tree.map(jnp.zeros_like, self)
         new_self: Self = eqx.tree_at(lambda m: m.J, zero_update, dJ)
         return new_self